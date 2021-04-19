093614c0e959a46094bd3d23ca8eca8ea9bc9a26
Archhhi <brabus-20@mail.ru>
2021-04-19 10:54:46 +0300

    Add HW6: Completed

--- src/p2-homeworks/h4/common/c1-SuperInputText/SuperInputText.tsx
+++ src/p2-homeworks/h4/common/c1-SuperInputText/SuperInputText.tsx
@@ -24,7 +24,6 @@ const SuperInputText: React.FC<SuperInputTextPropsType> = (
     ...restProps// все остальные пропсы попадут в объект restProps
   }
 ) => {
-  debugger
   const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
     onChange // если есть пропс onChange
     && onChange(e) // то передать ему е (поскольку onChange не обязателен)
