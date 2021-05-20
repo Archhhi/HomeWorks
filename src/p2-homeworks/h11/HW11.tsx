c6756579c08c1c795e1a8aa630ebf5477fd95bb3
Archhhi <brabus-20@mail.ru>
2021-05-20 15:35:50 +0300

    HW11 - completed

--- src/p2-homeworks/h11/HW11.tsx
+++ src/p2-homeworks/h11/HW11.tsx
@@ -3,37 +3,44 @@ import SuperRange from "./common/c7-SuperRange/SuperRange";
 import SuperDoubleRange from "./common/c8-SuperDoubleRange/SuperDoubleRange";
 
 function HW11() {
-    const [value1, setValue1] = useState(0);
-    const [value2, setValue2] = useState(100);
+  const [value1, setValue1] = useState(0);
+  const [value2, setValue2] = useState<any>(100);
 
-    return (
-        <div>
-            <hr/>
-            homeworks 11
+  const onChangeSuperDoubleRange = (value: [number, number]) => {
+    setValue1(value[0])
+    setValue2(value[1])
+  }
 
-            {/*should work (должно работать)*/}
-            <div>
-                <span>{value1}</span>
-                <SuperRange
-                    // сделать так чтоб value1 изменялось
-                />
-            </div>
+  return (
+    <div>
+      <hr/>
+      homeworks 11
 
-            <div>
-                <span>{value1}</span>
-                <SuperDoubleRange
-                    // сделать так чтоб value1 и value2 изменялось
-                />
-                <span>{value2}</span>
-            </div>
+      {/*should work (должно работать)*/}
+      <div>
+        <span>{value1}</span>
+        <SuperRange
+          onChangeRange={setValue1}
+        />
+      </div>
 
-            <hr/>
-            {/*для личного творчества, могу проверить*/}
-            {/*<AlternativeSuperRange/>*/}
-            {/*<AlternativeSuperDoubleRange/>*/}
-            <hr/>
-        </div>
-    );
+      <div>
+        <span>{value1}</span>
+        <SuperDoubleRange
+          value={[value1, value2]}
+          //@ts-ignore
+          onChangeRange={onChangeSuperDoubleRange}
+        />
+        <span>{value2}</span>
+      </div>
+
+      <hr/>
+      {/*для личного творчества, могу проверить*/}
+      {/*<AlternativeSuperRange/>*/}
+      {/*<AlternativeSuperDoubleRange/>*/}
+      <hr/>
+    </div>
+  );
 }
 
 export default HW11;
--- src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
+++ src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
@@ -1,24 +1,35 @@
 import React from "react";
+import s from "./SuperDoubleRange.module.css";
+import {Slider} from '@material-ui/core'
 
 type SuperDoubleRangePropsType = {
-    onChangeRange?: (value: [number, number]) => void
-    value?: [number, number]
-    // min, max, step, disable, ...
+  onChangeRange?: (newValue: number | number[]) => void
+  value?: [number, number]
+  // min, max, step, disable, ...
 }
 
 const SuperDoubleRange: React.FC<SuperDoubleRangePropsType> = (
-    {
-        onChangeRange, value,
-        // min, max, step, disable, ...
-    }
+  {
+    onChangeRange, value,
+    // min, max, step, disable, ...
+  }
 ) => {
-    // сделать самому, можно подключать библиотеки
+  // сделать самому, можно подключать библиотеки
+  const handleChange = (event: any, newValue: number | number[]) => {
+    onChangeRange && onChangeRange(newValue);
+  };
 
-    return (
-        <>
-            DoubleRange
-        </>
-    );
+  return (
+    <div className={s.polzunokContainer}>
+      <Slider
+        style={{width: 300, marginTop: 0, marginLeft: 300}}
+        onChange={handleChange}
+        value={value}
+        valueLabelDisplay="auto"
+        aria-labelledby="range-slider"
+      />
+    </div>
+  );
 }
 
 export default SuperDoubleRange;
